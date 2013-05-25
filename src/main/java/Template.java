2afe73edc45805e353d109dfa39e3b0fb0a2a6b1
Arron Green <arrong@gmail.com>
2013-05-25 00:19:05 -0400

    initial commit

---
+++ src/main/java/Template.java
@@ -0,0 +1,116 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Template extends Configured implements Tool
+{
+    private static final Logger logger = LoggerFactory.getLogger(Template.class);
+
+    public static class MapClass extends Mapper<LongWritable, Text, Text, LongWritable>
+    {
+        private final static LongWritable one = new LongWritable(1);
+        private Text word = new Text();
+
+        @Override
+        protected void setup(Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+                throws IOException, InterruptedException
+        {
+            super.setup(context);
+        }
+
+        @Override
+        public void map(LongWritable key, Text text, Mapper<LongWritable, Text, Text, LongWritable>.Context
+                context) throws IOException, InterruptedException
+        {
+            //example map code
+            /*
+            String regex = context.getConfiguration().get("wordcount.regex", "[A-Za-z']+");
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(value.toString());
+
+            while(m.find()){
+                word.set(m.group().toLowerCase());
+                context.write(word, one);
+            }
+            */
+        }
+    }
+
+    public static class ReduceClass extends Reducer<Text, LongWritable, Text, LongWritable>
+    {
+      public ReduceClass(){}
+
+        @Override
+        protected void setup(Reducer<Text, LongWritable, Text, LongWritable>.Context context)
+                throws IOException, InterruptedException
+        {
+            super.setup(context);
+        }
+
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context)
+                throws IOException, InterruptedException
+        {
+            //example reduce code
+            /*
+            long sum = 0;
+            for(LongWritable val : values){
+                sum += val.get();
+            }
+            context.write(key, new LongWritable(sum));
+            */
+        }
+    }
+
+    @Override
+    public int run(String[] args) throws IOException, InterruptedException, ClassNotFoundException
+    {
+        logger.warn("Starting map/reduce job");
+        Configuration conf = new Configuration();
+        Job job = new Job(conf);
+
+        job.setJarByClass(Template.class);
+        job.setMapperClass(MapClass.class);
+        job.setReducerClass(ReduceClass.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        Path outPath = new Path(args[1]);
+        FileOutputFormat.setOutputPath(job, outPath);
+
+        //this will delete the path before it starts, good for debugging
+        outPath.getFileSystem(conf).delete(outPath, true);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        int res = ToolRunner.run(new Configuration(), new Template(), args);
+        System.exit(res);
+    }
+
+}
+
+
